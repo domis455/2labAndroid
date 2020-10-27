f6002a713a5b9b70da2f0b4bbd6dae66fa28f504
domis455 <72032371+domis455@users.noreply.github.com>
2020-10-27 22:11:05 +0200

    įkeliama

--- app/src/main/java/com/example/a2labandroid/MainActivity.java
+++ app/src/main/java/com/example/a2labandroid/MainActivity.java
@@ -5,22 +5,34 @@ import android.view.View;
 import android.os.Bundle;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.ArrayAdapter;
-import static com.example.a2labandroid.R.array.selection_options;
+import android.util.Log;
+
+import java.text.BreakIterator;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner spCounterOption;
+    private EditText edEnteredText;
+    private TextView tvResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    this.spCounterOption = findViewById(R.id.spCounterOption);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Log.i("Routine", "onCreate =>");
 
-      ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, selection_options, android.R.layout.simple_spinner_item);
+    this.spCounterOption = findViewById(R.id.spCounterOption);
+    this.edEnteredText = findViewById(R.id.edEnteredText);
+    this.tvResult = findViewById(R.id.tvResult);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.selection_options, android.R.layout.simple_spinner_item);
       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
       this.spCounterOption.setAdapter(adapter);
 
@@ -28,11 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void btnSelection(View view) {
+    public void btnSelectionClick(View view) {
         Toast.makeText(this, this.spCounterOption.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+
         if(this.spCounterOption.getSelectedItem().toString().equalsIgnoreCase(getString(R.string.selection_chars)))
         {
-
+            this.tvResult.setText(String.valueOf(this.edEnteredText.getText().toString().length()));
+        }
+        else
+        {
+            Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT);
         }
     }
 }
